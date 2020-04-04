34b59b382a9955994e08041f8983ffeee6afc8de
Helder Falcão <helderfalcaob@gmail.com>
2020-04-04 15:17:05 -0300

    Fix connection handling

--- src/dao/db.js
+++ src/dao/db.js
@@ -1,13 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 module.exports = function () {
-    var mongoURI =
-        process.env['MLAB_CONNECTION'] ?
-            process.env['MLAB_CONNECTION'] : "mongodb://localhost:27017/solidarizar";
-    mongoose.connect(mongoURI)
-        .connection.on('error', function (err) {
-            console.log(err.message);
-        }).once('open', function () {
-            console.log("mongodb connection open");
-        });
-    mongoose.Promise = require('bluebird');
-}
+  var mongoURI = process.env["MLAB_CONNECTION"]
+    ? process.env["MLAB_CONNECTION"]
+    : "mongodb://localhost:27017/solidarizar";
+  mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  mongoose.connection
+    .on("error", function (err) {
+      console.log(err.message);
+    })
+    .once("open", function () {
+      console.log("mongodb connection open");
+    });
+  mongoose.Promise = require("bluebird");
+};
